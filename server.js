62d1e72262defb52a035f972e90cca81836f4845
Nehal <kadyannehal333@gmail.com>
2023-12-21 19:32:04 +0530

    commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json("Hello world")
+})
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000")
+})
