08a9a4c8ff943429a73de59641cf30b9aa47331b
Nehal <kadyannehal333@gmail.com>
2023-12-21 20:19:25 +0530

    added

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     res.status(200).json("Hello world")
 })
 
+app.get("/students", (req, res) => {
+    res.status(200).json({name: "Nehal Kadyan"})
+})
+
 app.listen(3000, () => {
     console.log("Server listening on port 3000")
 })
